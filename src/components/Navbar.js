cfd7cc10d6de15820675731f81599f57de323288
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-04-29 17:33:20 +0530

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import React from 'react'
 export default function Navbar() {
   return (
     <>
+        <div>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
                 <a className="navbar-brand" href="/">converText</a>
@@ -15,7 +16,7 @@ export default function Navbar() {
                             <a className="nav-link active" aria-current="page" href="/">Home</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link "  href="/">Home</a>
+                            <a className="nav-link --bs-dark-border-subtle "  href="/">Home</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="/">Link</a>
@@ -24,6 +25,7 @@ export default function Navbar() {
                 </div>
             </div>
         </nav>
+    </div>
     </>
   )
 }
