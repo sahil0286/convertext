162f307b32c55915b2505024fd236cf78d27bafb
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-04-29 21:03:16 +0530

    new fix

--- src/components/Inputarea.js
+++ src/components/Inputarea.js
@@ -8,24 +8,26 @@ export default function Inputarea(props) {
             <label htmlFor="exampleFormControlTextarea1" className="form-label">{props.inpSubHeading}</label>
             <textarea className="form-control" id="exampleFormControlTextarea1" rows="7"></textarea>
         </div>
-        <div>
-            <button type="button" className="btn btn-primary btn-sm mx-2">Small button</button>
-            <button type="button" className="btn btn-primary btn-sm mx-2">Small button</button>
-            <button type="button" className="btn btn-primary btn-sm mx-2">Small button</button>
-            <button type="button" className="btn btn-primary btn-sm mx-2">Small button</button>
-            <button type="button" className="btn btn-primary btn-sm mx-2">Small button</button>
-            <button type="button" className="btn btn-primary btn-sm mx-2">Small button</button>
+        <div className='container'>
+            <button type="button" className="btn btn-primary btn-sm mx-2 my-2">Small button</button>
+            <button type="button" className="btn btn-primary btn-sm mx-2 my-2">Small button</button>
+            <button type="button" className="btn btn-primary btn-sm mx-2 my-2">Small button</button>
+            <button type="button" className="btn btn-primary btn-sm mx-2 my-2">Small button</button>
+            <button type="button" className="btn btn-primary btn-sm mx-2 my-2">Small button</button>
+            <button type="button" className="btn btn-primary btn-sm mx-2 my-2">Small button</button>
         </div>
         {/* <p>`Character Count: ${charCnt} | Word Count: ${wordCnt} | Line Count: ${lineCnt}`</p> */}
-        <div className='d-flex justify-content-center mt-2'>
-            <button type="button" className="btn btn-primary btn-sm mx-2">Small button</button>
-            <button type="button" className="btn btn-primary btn-sm mx-2">Small button</button>
-            <button type="button" className="btn btn-primary btn-sm mx-2">Small button</button>
-            <button type="button" className="btn btn-primary btn-sm mx-2">Small button</button>
-            <button type="button" className="btn btn-primary btn-sm mx-2">Small button</button>
-            <button type="button" className="btn btn-primary btn-sm mx-2">Small button</button>
+        <div className='container'>
+            <div className='container d-flex justify-content-center mt-2'>
+            <button type="button" className="btn btn-primary btn-sm mx-2 my-2">Small button</button>
+            <button type="button" className="btn btn-primary btn-sm mx-2 my-2">Small button</button>
+            <button type="button" className="btn btn-primary btn-sm mx-2 my-2">Small button</button>
+            <button type="button" className="btn btn-primary btn-sm mx-2 my-2">Small button</button>
+            <button type="button" className="btn btn-primary btn-sm mx-2 my-2">Small button</button>
+            <button type="button" className="btn btn-primary btn-sm mx-2 my-2">Small button</button>
+            </div>
         </div>
-        <p className='text-center mt-2'>Copyright ©2023-2030 converText Ltd | Concept by <a href="/">Sahil Fakir</a> | <a href="/">Privacy Policy</a> | <a href="/">Terms of Use</a> | <a href="/">Sitemap</a> | <a href="/">About</a> </p>
+        <p className='container text-center mt-2'>Copyright ©2023-2030 converText Ltd | Concept by <a href="/">Sahil Fakir</a> | <a href="/">Privacy Policy</a> | <a href="/">Terms of Use</a> | <a href="/">Sitemap</a> | <a href="/">About</a> </p>
     </div>
   )
 }
