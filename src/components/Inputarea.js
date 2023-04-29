d196155e40f7519114f6d11d5af379fc8c735d16
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-04-29 20:18:05 +0530

    extra info added

--- src/components/Inputarea.js
+++ src/components/Inputarea.js
@@ -8,7 +8,7 @@ export default function Inputarea(props) {
             <label htmlFor="exampleFormControlTextarea1" className="form-label">{props.inpSubHeading}</label>
             <textarea className="form-control" id="exampleFormControlTextarea1" rows="7"></textarea>
         </div>
-        <div className='d-flex justify-content-center'>
+        <div>
             <button type="button" className="btn btn-primary btn-sm mx-2">Small button</button>
             <button type="button" className="btn btn-primary btn-sm mx-2">Small button</button>
             <button type="button" className="btn btn-primary btn-sm mx-2">Small button</button>
@@ -16,6 +16,16 @@ export default function Inputarea(props) {
             <button type="button" className="btn btn-primary btn-sm mx-2">Small button</button>
             <button type="button" className="btn btn-primary btn-sm mx-2">Small button</button>
         </div>
+        {/* <p>`Character Count: ${charCnt} | Word Count: ${wordCnt} | Line Count: ${lineCnt}`</p> */}
+        <div className='d-flex justify-content-center mt-2'>
+            <button type="button" className="btn btn-primary btn-sm mx-2">Small button</button>
+            <button type="button" className="btn btn-primary btn-sm mx-2">Small button</button>
+            <button type="button" className="btn btn-primary btn-sm mx-2">Small button</button>
+            <button type="button" className="btn btn-primary btn-sm mx-2">Small button</button>
+            <button type="button" className="btn btn-primary btn-sm mx-2">Small button</button>
+            <button type="button" className="btn btn-primary btn-sm mx-2">Small button</button>
+        </div>
+        <p className='text-center mt-2'>Copyright ©2023-2030 converText Ltd | Concept by <a href="/">Sahil Fakir</a> | <a href="/">Privacy Policy</a> | <a href="/">Terms of Use</a> | <a href="/">Sitemap</a> | <a href="/">About</a> </p>
     </div>
   )
 }
