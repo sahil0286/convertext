a8c1dd17aca141023b51866df7e1cc964698e421
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-04-30 22:59:39 +0530

    Update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import {
+    BrowserRouter as  Link } from "react-router-dom";
 
 export default function Navbar(props) {
   return (
@@ -6,26 +8,26 @@ export default function Navbar(props) {
         <div className='continer'>
             <nav className="navbar navbar-expand-lg bg-body-tertiary d-flex justify-content-evenly">
             <div className="container-fluid">
-                <a className="navbar-brand " href="/">{props.title}</a>
+                <Link className="navbar-brand " to="/">{props.title}</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link" href="/">{props.sbTitle1}</a>
+                            <Link className="nav-link" to="/NewCoding">{props.sbTitle1}</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/">{props.sbTitle2}</a>
+                            <Link className="nav-link" to="/">{props.sbTitle2}</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/">{props.sbTitle3}</a>
+                            <Link className="nav-link" to="/">{props.sbTitle3}</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/">{props.sbTitle4}</a>
+                            <Link className="nav-link" to="/">{props.sbTitle4}</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/">{props.sbTitle5}</a>
+                            <Link className="nav-link" to="/">{props.sbTitle5}</Link>
                         </li>
                     </ul>
                     <div className="form-check form-switch">
