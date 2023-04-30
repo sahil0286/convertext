3eb74d26891b1cef1b67e35bbb8d61fa51472059
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-04-30 23:19:46 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
         <Ad1 ad1="https://global-uploads.webflow.com/5f6cc9cd16d59d990c8fca33/61d30ac833d72078f141fb47_team-seas-raised-30-million-come-back.jpg" ad2="https://u4d2z7k9.rocketcdn.me/wp-content/uploads/2022/01/rsz_191026-theoceancleanup_malaysia-1099294.jpg"/>
         <Footer/>
           <Switch>
-            <Route path="/NewCoding">
+            <Route path="/newcoding">
               <NewCoding/>
             </Route>
           </Switch>
