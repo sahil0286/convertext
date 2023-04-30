40a383df7c03e1ca35fe734ef91645894c9b0a95
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-04-30 18:18:26 +0530

    Fix Ad1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
+import Ad1 from './components/Ad1';
 import Inputarea from './components/Inputarea';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-      <Navbar title="converText"/>
-      <Inputarea inpHeading="Accidentally left the caps lock on and typed something, but can't be bothered to start again and retype it all?" inpSubHeading="Simply enter your text and choose the case you want to convert it to."/>
-    </>
+        <Navbar title="converText"/>
+        <Inputarea inpHeading="Accidentally left the caps lock on and typed something, but can't be bothered to start again and retype it all?" inpSubHeading="Simply enter your text and choose the case you want to convert it to."/>
+        <Ad1/>
+      </>
   );
 }
 
--- src/components/Ad1.js
+++ src/components/Ad1.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 export default function Ad1() {
   return (
-    <>
-        <img src="https://global-uploads.webflow.com/5f6cc9cd16d59d990c8fca33/61d30ac833d72078f141fb47_team-seas-raised-30-million-come-back.jpg" class="img-fluid" alt="Ad_No_1"></img>
+    <>  <div className='container'>
+            <a href="https://teamseas.org/" className='rounded float-start'>
+                <img style={{height:"250px"}} src="https://global-uploads.webflow.com/5f6cc9cd16d59d990c8fca33/61d30ac833d72078f141fb47_team-seas-raised-30-million-come-back.jpg" className="rounded" alt="Ad_No_1"/>
+            </a>
+            <a href="https://teamseas.org/" className='rounded float-end'>
+                <img style={{height:"250px"}} src="https://global-uploads.webflow.com/5f6cc9cd16d59d990c8fca33/61d30ac833d72078f141fb47_team-seas-raised-30-million-come-back.jpg" className="rounded" alt="Ad_No_1"/>
+            </a>
+            </div>
     </>
   )
 }
