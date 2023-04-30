c6b8bd1fcffc68f466b1db907ff5d35867bfb461
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-04-30 18:37:25 +0530

    Update ad

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <>
         <Navbar title="converText"/>
         <Inputarea inpHeading="Accidentally left the caps lock on and typed something, but can't be bothered to start again and retype it all?" inpSubHeading="Simply enter your text and choose the case you want to convert it to."/>
-        <Ad1/>
+        <Ad1 ad1="https://global-uploads.webflow.com/5f6cc9cd16d59d990c8fca33/61d30ac833d72078f141fb47_team-seas-raised-30-million-come-back.jpg" ad2="https://u4d2z7k9.rocketcdn.me/wp-content/uploads/2022/01/rsz_191026-theoceancleanup_malaysia-1099294.jpg"/>
       </>
   );
 }
