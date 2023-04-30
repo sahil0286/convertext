9efd1f9bd971b193ca5f3ef4177177cc06e30508
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-04-30 17:45:10 +0530

    Ad no 1

---
+++ src/components/Ad1.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Ad1() {
+  return (
+    <>
+        <img src="https://global-uploads.webflow.com/5f6cc9cd16d59d990c8fca33/61d30ac833d72078f141fb47_team-seas-raised-30-million-come-back.jpg" class="img-fluid" alt="Ad_No_1"></img>
+    </>
+  )
+}
