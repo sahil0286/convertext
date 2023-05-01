87c90dbeb732e31114f3826c34c9e98e946a4980
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-01 12:26:22 +0530

    update

--- src/components/Smalltext.js
+++ src/components/Smalltext.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React,{useState} from 'react'
+
 
 export default function Smalltext() {
     const [text, setText] = useState("")
@@ -12,7 +13,6 @@ export default function Smalltext() {
     <>
         <div className="container input-group mb-3 mt-3">
             <textarea className="form-control" value={text} id="text" rows="8" onChange={textBox} placeholder='Type or paste your content here'></textarea>
-            <textarea className="form-control"  id="text" rows="8" placeholder='Output'></textarea>
         </div>
     </>
   )
