7e4b5cf4256f38d83185478016552b80abb45342
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-01 15:55:01 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
           <Route path='/smalltextgenerator' element=
           {
             <div>
-              <Inputbox inpHeading="Small Text Generator" inpSubHeading="This small text generator is a handy online tool, where you can convert standard text (whether that’s capitals, small letters and more) into tiny text. The tiny text options are ‘small caps’ and ‘superscript’. Find out how to use the tiny text generator below." btName="Convert Small Text"/>
+              <Inputbox inpHeading="Small Text Generator" inpSubHeading="This small text generator is a handy online tool, where you can convert standard text (whether that’s capitals, small letters and more) into tiny text. The tiny text options are ‘small caps’ and ‘superscript’. Find out how to use the tiny text generator below." btName="Convert To Small Text"/>
               <Ad1 ad1="https://global-uploads.webflow.com/5f6cc9cd16d59d990c8fca33/61d30ac833d72078f141fb47_team-seas-raised-30-million-come-back.jpg" ad2="https://u4d2z7k9.rocketcdn.me/wp-content/uploads/2022/01/rsz_191026-theoceancleanup_malaysia-1099294.jpg"/>
               <Footer/>
             </div>
