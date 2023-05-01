0c15b5a3b28ec811cbb4ccf888bcc3804e868e1a
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-01 11:46:13 +0530

    Updates

--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ function App() {
   return (
     <>
         <Navbar title="converText" sbTitle1="Small Text Generator"/>
-        <Footer/>
         <Routes>
           <Route path='/' element={
             <div>
               <Inputarea inpHeading="Accidentally left the caps lock on and typed something, but can't be bothered to start again and retype it all?" inpSubHeading="Simply enter your text and choose the case you want to convert it to."/>
               <Ad1 ad1="https://global-uploads.webflow.com/5f6cc9cd16d59d990c8fca33/61d30ac833d72078f141fb47_team-seas-raised-30-million-come-back.jpg" ad2="https://u4d2z7k9.rocketcdn.me/wp-content/uploads/2022/01/rsz_191026-theoceancleanup_malaysia-1099294.jpg"/>
+              <Footer/>
             </div>
           }/>
           <Route path='/smalltextgenerator' element={<Smalltext/>}/>
