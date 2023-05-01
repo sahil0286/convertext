f2d65690806cdddc997240446b9d8da59c97bca0
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-01 10:42:24 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import A from './components/A';
 import Ad1 from './components/Ad1';
@@ -13,6 +14,10 @@ function App() {
         <Inputarea inpHeading="Accidentally left the caps lock on and typed something, but can't be bothered to start again and retype it all?" inpSubHeading="Simply enter your text and choose the case you want to convert it to."/>
         <Ad1 ad1="https://global-uploads.webflow.com/5f6cc9cd16d59d990c8fca33/61d30ac833d72078f141fb47_team-seas-raised-30-million-come-back.jpg" ad2="https://u4d2z7k9.rocketcdn.me/wp-content/uploads/2022/01/rsz_191026-theoceancleanup_malaysia-1099294.jpg"/>
         <Footer/>
+        <Routes>
+          <Route path='/newcoding' element={<NewCoding/>}/>
+          <Route path='/a' element={<A/>}/>
+        </Routes>
     </>
   );
 }
