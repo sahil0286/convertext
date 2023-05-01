a7ff9c975e1cdcaa345be9840bc0e2df9e400620
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-01 19:38:08 +0530

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar(props) {
   return (
     <>
         <div className='continer'>
-            <nav className={`navbar navbar-expand-lg bg-body-${props.mode} d-flex justify-content-evenly`}>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary d-flex justify-content-evenly">
             <div className="container-fluid">
                 <Link className="navbar-brand " to="/">{props.title}</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
